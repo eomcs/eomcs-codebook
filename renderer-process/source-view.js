b8d8d3efe7c5476bd1f3b10e6b422289cce06edf
Jinyoung Eom <eomjinyoung@eomjin-yeong-ui-Mac-Pro.local>
2017-10-11 01:27:53 +0900

    .

--- renderer-process/source-view.js
+++ renderer-process/source-view.js
@@ -19,35 +19,15 @@ let markdownRenderer = new Remarkable({
   // Modify the generated HTML by highlighting the code directly
   // by Prismjs 
   highlight: function (str, lang) {
-    var language = !lang ? 'markup' : lang;
-
-    if (!Prism.languages[language]) {
-      require('prismjs/components/prism-' + language); 
-    }
-
+    loadPrismjsLanguagePlugin(lang)
+    var language =  isSupportedLanguage(lang) ? lang : 'markup';
+    
     try {
       return Prism.highlight(str, Prism.languages[language]);
     } catch (err) {}
 
     return ''; // use external default escaping
   }, 
-
-  // by Highlight.js
-  /*
-  highlight: function (str, lang) {
-    if (lang && hljs.getLanguage(lang)) {
-        try {
-            return hljs.highlight(lang, str).value;
-        } catch (err) {}
-    }
-
-    try {
-        return hljs.highlightAuto(str).value;
-    } catch (err) {}
-
-    return ''; // use external default escaping
-  }
-  */
 })
 
 const section = document.querySelector('#source-view-section')
@@ -61,43 +41,98 @@ section.addEventListener('changedSourceFile', e => {
   let lang = langExt(path.extname(e.detail))
 
 
-  fs.readFile(sourceFilePath, 'utf8', (err, data) => {
+  fs.readFile(sourceFilePath, 'utf8', (err, sourceCode) => {
     if (err) throw err;
 
-    var markdownSource = ''
-    var isCodeBlock = false
+    var markdownCode = toMarkdownCode(sourceCode, lang)
+    sourceFileView.innerHTML = markdownRenderer.render(markdownCode)
 
-    data.split('\n').forEach((value, index) => {
-      if (value.startsWith('//:')) {
-        if (isCodeBlock) { // 코드 블록을 끝낸다. 
-          markdownSource += '```\n'
-          isCodeBlock = false
-        }
-        markdownSource += value.replace('//:', '') + '\n'
+    // terminal 코드에 대해 스타일 적용하기
+    styleTerminalCode(sourceFileView)
 
-      } else {
-        if (!isCodeBlock) { // 코드 블록을 시작한다.
-          markdownSource += '```' + lang + '\n'
-          isCodeBlock = true
-        }
-        markdownSource += value + '\n'
-      }
-    })
-    if (isCodeBlock) { // 코드 블록을 끝낸다. 
-      markdownSource += '```\n'
-    }
+    // 일반 코드에 대해 스타일 적용하기
+    stylePlainCode(sourceFileView)
 
-    var re = new RegExp('```' + lang + '\\s*```', 'g')
-    markdownSource = markdownSource.replace(re, '')
-    sourceFileView.innerHTML = markdownRenderer.render(markdownSource)
-
-    var codeList = sourceFileView.querySelectorAll('pre code:not([class|="language"])')
-    for (var code of codeList) {
-      code.parentElement.classList.add('terminal')
-    }
+    // 소스코드 줄 번호 적용하기
+    styleLineNumbers(sourceFileView, lang)
+    
   });
 })
 
+function toMarkdownCode(sourceCode, lang) {
+  var markdownCode = ''
+  var isCodeBlock = false
+
+  sourceCode.split('\n').forEach((value, index) => {
+    if (value.startsWith('//:')) {
+      if (isCodeBlock) { // 코드 블록을 끝낸다. 
+        markdownCode += '```\n'
+        isCodeBlock = false
+      }
+      markdownCode += value.replace('//:', '') + '\n'
+
+    } else {
+      if (!isCodeBlock) { // 코드 블록을 시작한다.
+        markdownCode += '```' + lang + '\n'
+        isCodeBlock = true
+      }
+      markdownCode += value + '\n'
+    }
+  })
+  if (isCodeBlock) { // 코드 블록을 끝낸다. 
+    markdownCode += '```\n'
+  }
+
+  markdownCode = removeEmptyCode(markdownCode)
+
+  return markdownCode
+}
+
+function removeEmptyCode(markdownCode) {
+  var re = new RegExp('```\\w+\\s*```', 'g')
+  return markdownCode.replace(re, '')
+}
+
+function styleTerminalCode(element) {
+  var codeList = element.querySelectorAll('pre code.language-terminal')
+  for (var code of codeList) {
+    code.parentElement.classList.add('terminal')
+  }
+}
+
+function stylePlainCode(element) {
+  var codeList = element.querySelectorAll('pre code:not([class|="language"])')
+  for (var code of codeList) {
+    code.parentElement.classList.add('plain-code')
+  }
+}
+
+function styleLineNumbers(element, lang) {
+  var codeList = element.querySelectorAll('pre code.language-' + lang)
+  for (var code of codeList) {
+    code.parentElement.classList.add('line-numbers')
+  }
+  require('prismjs/plugins/line-numbers/prism-line-numbers')
+}
+
+
 function setSourceViewTitle(title) {
   sourceFilename.innerHTML = title
+}
+
+function loadPrismjsLanguagePlugin(language) {
+  if (!Prism.languages[language]) {
+    try {
+      require('prismjs/components/prism-' + language)
+    } catch (err) {
+      return false
+    }
+  }
+  return true
+}
+
+function isSupportedLanguage(lang) {
+  if (Prism.languages[lang])
+    return true
+  return false
 }
